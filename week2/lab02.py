5ad0cf8b99759be01895c15eb2de445373965cf1
ColinPorter <colin.jm.porter@gmail.com>
2025-01-17 11:25:37 -0500

    lab2 end

--- week2/lab02.py
+++ week2/lab02.py
@@ -15,9 +15,25 @@ def number_guessing_game():
 
     while attempts < max_attempts:
         try:
+            userGuess = int(input("Guess a number between 1 and 100: "))
+            attempts += 1
+            if userGuess == targetNumber:
+                print(f"Congratulations, you guessed it in {attempts} tries!")
+            elif userGuess < targetNumber:
+                if (userGuess - targetNumber) <= 5:
+                    print("Too low, you're really close!")
+                else:
+                    print("Too low, try again.")
+            else:
+                if (targetNumber - userGuess) >= 5:
+                    print("Too high, you're really close!")
+                else:
+                    print("Too high, try again.")
             return True
-        except:
-            print("")
-
+        except ValueError:
+            print("Invalid input. Please try again, between 1 and 100.")
+    print(f"Game Over. The target number is {targetNumber}.")
     play = input()
     return True
+
+number_guessing_game()
